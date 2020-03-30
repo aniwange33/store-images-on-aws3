4c31f76e749573805551ef277e3abaaf0623462a
aniwange <terteseamos@gmail.com>
2020-03-30 11:24:10 +0100

    feat:  AWS  S3  API  dependence applied to project

---
+++ src/main/java/com/amos/awss3tutorial/Awss3AppApplication.java
@@ -0,0 +1,13 @@
+package com.amos.awss3tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Awss3AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Awss3AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amos/awss3tutorial/Awss3AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amos.awss3tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Awss3AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
